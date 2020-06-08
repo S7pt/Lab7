c86fcaa2e39521cecc38bce14f03f13f7d8a9fdc
S7pt <chaos160702@gmail.com>
2020-06-08 18:58:48 +0300

    Task1

---
+++ Lab7/Book.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lab7
+{
+    public class Book : IComparable
+    {
+        public int price;
+        public int pagesCount;
+        public string author;
+        public string genre;
+        public int yearOfProduction;
+
+        public Book(int price, int pagesCount, string author, string genre, int yearOfProduction)
+        {
+            this.price = price;
+            this.pagesCount = pagesCount;
+            this.author = author;
+            this.genre = genre;
+            this.yearOfProduction = yearOfProduction;
+        }
+
+        public int CompareTo(object other)
+        {
+            Book book = (Book) other;
+            return price - book.price;
+        }
+
+        
+
+        public override string ToString()
+        {
+            return "|" + price + "|" + pagesCount + "|" + author + "|" + genre + "|" + yearOfProduction + "|";
+        }
+
+        public class Books : IEnumerable
+        {
+           public Book[] bookArray;
+           private int count = 0;
+
+            public Books(int count)
+            {
+                bookArray = new Book[count];
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return bookArray.GetEnumerator();
+            }
+
+            public void Add(Book item)
+            {
+                if (count == bookArray.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                bookArray[count] = item;
+                count++;
+            }
+            public class CompareClass:IComparer<Book>
+            {
+                public int Compare(Book x, Book y)
+                {
+                    int comparer = x.price - y.price;
+                    if (comparer != 0)
+                    {
+                        return comparer;
+                    }
+
+                    comparer = x.pagesCount - y.pagesCount;
+                    return comparer;
+                }
+            }
+
+            public void SortByPriceAndPagesCount()
+            {
+                List<Book> bookList=new List<Book>();
+                foreach (Book bookUnit in bookArray)
+                {
+                    bookList.Add(bookUnit);
+                }
+                CompareClass compareClass=new CompareClass();
+                bookList.Sort(compareClass);
+                int i = 0;
+                foreach (Book bookUnit in bookList)
+                {
+                    bookArray[i] = bookUnit;
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type the number of elements in list: ");
+            int counter = int.Parse(Console.ReadLine());
+            Book.Books bookArray = new Book.Books(counter);
+            for (int i = 0; i < bookArray.bookArray.Length; i++)
+            {
+                Console.Write(i+1+" Book price:");
+                int priceCounter = int.Parse(Console.ReadLine());
+                Console.Write(i+1+" Book pages:");
+                int pagesCounter = int.Parse(Console.ReadLine());
+                Console.Write(i + 1 + " Book author:");
+                string author = Console.ReadLine();
+                Console.Write(i + 1 + " Book genre:");
+                string genre = Console.ReadLine();
+                Console.Write(i + 1 + " Book year of creating:");
+                int year = int.Parse(Console.ReadLine());
+                bookArray.Add(new Book(priceCounter,pagesCounter,author,genre,year));
+            }
+            Console.WriteLine("Before sorting:");
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (Book bookUnit in bookArray)
+            {
+                Console.WriteLine(bookUnit);
+            }
+
+            bookArray.SortByPriceAndPagesCount();
+            Console.WriteLine("After sorting:");
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (Book bookUnit in bookArray)
+            {
+                Console.WriteLine(bookUnit);
+            }
+        }
+    }
+}
